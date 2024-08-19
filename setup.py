c41d34e1f0a1a4953770fbc578a9c7bce76151ad
Yang Mu <140584393+YuhuYang@users.noreply.github.com>
2024-08-19 22:54:05 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
  
 setuptools.setup(
     name="quantling", 
-    version="0.0.1",   
+    version="0.1.0",   
     author="Mu Yang",    
     author_email="yuhuyang@163.com",   
     description="A package for quantiative linguistics",
